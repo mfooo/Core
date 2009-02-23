45338fe44e84225b841808da88f9e62ee8ae310a
pasdVn <pasdVn3@gmx.de>
2009-02-23 19:44:39 +0100

    [OV11] Added some pet talents (dummy aura procs).
    * "Guard Dog", 53178 (+ranks)
    * "Improved Cower", 53180 (+ranks), already in RSA, but sql missing
    * "Silverback", 62764 (+ranks), already in RSA, but sql missing

---
+++ sql/custom/[OV11]_spell_proc.sql
@@ -0,0 +1,18 @@
+/*Improved Cower*/
+DELETE FROM `spell_proc_event` WHERE entry IN (53180, 53181);
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMaskA0`, `SpellFamilyMaskA1`, `SpellFamilyMaskA2`, `SpellFamilyMaskB0`, `SpellFamilyMaskB1`, `SpellFamilyMaskB2`, `SpellFamilyMaskC0`, `SpellFamilyMaskC1`, `SpellFamilyMaskC2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+('53180', '0', '9', '0', '0', '0', '268435456', '0', '0', '0', '0', '0', '16', '0', '0', '0', '0'),
+('53181', '0', '9', '0', '0', '0', '268435456', '0', '0', '0', '0', '0', '16', '0', '0', '0', '0');
+
+/*Guard Dog*/
+DELETE FROM `spell_proc_event` WHERE entry IN (53178, 53179);
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMaskA0`, `SpellFamilyMaskA1`, `SpellFamilyMaskA2`, `SpellFamilyMaskB0`, `SpellFamilyMaskB1`, `SpellFamilyMaskB2`, `SpellFamilyMaskC0`, `SpellFamilyMaskC1`, `SpellFamilyMaskC2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+('53178', '0', '9', '0', '0', '0', '268435456', '0', '0', '0', '0', '0', '65536', '0', '0', '0', '0'),
+('53179', '0', '9', '0', '0', '0', '268435456', '0', '0', '0', '0', '0', '65536', '0', '0', '0', '0');
+
+/*Silverback*/
+DELETE FROM `spell_proc_event` WHERE entry IN (62764, 62765);
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMaskA0`, `SpellFamilyMaskA1`, `SpellFamilyMaskA2`, `SpellFamilyMaskB0`, `SpellFamilyMaskB1`, `SpellFamilyMaskB2`, `SpellFamilyMaskC0`, `SpellFamilyMaskC1`, `SpellFamilyMaskC2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+('62764', '0', '9', '0', '0', '0', '268435456', '0', '0', '0', '0', '0', '65536', '0', '0', '0', '0'),
+('62765', '0', '9', '0', '0', '0', '268435456', '0', '0', '0', '0', '0', '65536', '0', '0', '0', '0');
+
