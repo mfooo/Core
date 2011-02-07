7bab101ff5b7f4f4894e3bd7f16470f5da1841ff
pasdVn <pasdVn3@gmx.de>
2011-02-07 18:44:15 +0100

    [OV3] Fixed dummy effect of spell 23019.
    This fixes quest warlock 7853, although I'm not sure if this quests
    still exsists (at least in cataclysm it's gone).

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -1140,7 +1140,7 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
 
                     pGameObj->SetRespawnTime(creatureTarget->GetRespawnTime()-time(NULL));
                     pGameObj->SetOwnerGuid(m_caster->GetObjectGuid() );
-                    pGameObj->SetUInt32Value(GAMEOBJECT_LEVEL, m_caster->getLevel() );
+                    //pGameObj->SetUInt32Value(GAMEOBJECT_LEVEL, m_caster->getLevel() );
                     pGameObj->SetSpellId(m_spellInfo->Id);
 
                     creatureTarget->ForcedDespawn();
