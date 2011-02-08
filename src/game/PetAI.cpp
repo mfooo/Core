b2b862f732a2fcbda141009a545fb2246aacd544
pasdVn <pasdVn3@gmx.de>
2011-02-08 12:42:01 +0100

    [OV12] Unified Player and Creature cooldown handling.
    Also support cooldown-spellmods for pets.

--- src/game/PetAI.cpp
+++ src/game/PetAI.cpp
@@ -306,8 +306,6 @@ void PetAI::UpdateAI(const uint32 diff)
                     m_creature->SendCreateUpdateToPlayer( (Player*)owner );
             }
 
-            m_creature->AddCreatureSpellCooldown(spell->m_spellInfo->Id);
-
             spell->prepare(&targets);
         }
 
