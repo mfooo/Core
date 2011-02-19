736979ef2e8ce9bfd92fcb16893dffe37be5b30c
GMHAZE <iceis2@hotmail.com>
2011-02-19 10:41:59 -0500

    change in eoa  this isnt right yet

--- src/bindings/Infinity_SciptDev2/scripts/eastern_kingdoms/scarlet_enclave/ebon_hold.cpp
+++ src/bindings/Infinity_SciptDev2/scripts/eastern_kingdoms/scarlet_enclave/ebon_hold.cpp
@@ -1319,14 +1319,13 @@ struct MANGOS_DLL_DECL npc_eye_of_acherusAI : public ScriptedAI
 
         m_creature->RemoveAurasDueToSpell(530);
 
-		Player* owner = ObjectAccessor::FindPlayer(ownerGuid);
+        Player* owner = m_creature->GetMap()->GetPlayer(ownerGuid);;
 
         if(!owner)
             return;
 
-        owner->RemoveAurasDueToSpell(51852);
         owner->RemoveAurasDueToSpell(51923);
-		owner->RemoveAurasDueToSpell(51890);
+        owner->RemoveAurasDueToSpell(51852);
     }
 
     void MovementInform(uint32 uiType, uint32 uiPointId)
