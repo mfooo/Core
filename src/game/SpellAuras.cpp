6c649c0de4bbb47ec8c2553e29f1e70706dc8587
pasdVn <pasdVn3@gmx.de>
2011-02-22 22:29:45 +0100

    [OV16] Crashfix of the creature-cooldown patch.

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -8995,10 +8995,10 @@ void SpellAuraHolder::_RemoveSpellAuraHolder()
         }
 
         // reset cooldown state for spells
-        if ( GetSpellProto()->Attributes & SPELL_ATTR_DISABLED_WHILE_ACTIVE )
+        if (caster && GetSpellProto()->Attributes & SPELL_ATTR_DISABLED_WHILE_ACTIVE )
         {
             // always send for players
-            if(caster && caster->GetTypeId() == TYPEID_PLAYER)
+            if (caster->GetTypeId() == TYPEID_PLAYER)
             {
                 // note: item based cooldowns and cooldown spell mods with charges ignored (unknown existing cases)
                 ((Player*)caster)->SendCooldownEvent(GetSpellProto());
