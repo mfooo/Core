0cbe93a607fbfabcbbc4c6fe3aed4521e452784a
GMHAZE <iceis2@hotmail.com>
2011-02-26 23:20:37 -0500

    minor fix

--- src/bindings/Infinity_SciptDev2/scripts/kalimdor/caverns_of_time/hyjal/boss_archimonde.cpp
+++ src/bindings/Infinity_SciptDev2/scripts/kalimdor/caverns_of_time/hyjal/boss_archimonde.cpp
@@ -474,8 +474,8 @@ struct MANGOS_DLL_DECL boss_archimondeAI : public ScriptedAI
         if (Enraged)
         {
             if (HandOfDeathTimer < diff)
-			{
-                DoCastSpellIfCan(m_creature, SPELL_HAND_OF_DEATH, 0, 0);
+            {
+                DoCastSpellIfCan(m_creature->getVictim(), SPELL_HAND_OF_DEATH, 0, 0);
                 HandOfDeathTimer = 2000;
             }else HandOfDeathTimer -= diff;
             return;                                         // Don't do anything after this point.
