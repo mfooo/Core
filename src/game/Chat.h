e77d53504df48de9c629cdb3ebfc4b9e1bce1ecc
GMHAZE <iceis2@hotmail.com>
2011-03-03 14:36:34 -0500

    Implement .debug spellcoefs command Command show default calculated and DB stored coefficients for spell direct/dot heal/damage. by VladimirMangos

--- src/game/Chat.h
+++ src/game/Chat.h
@@ -194,6 +194,7 @@ class ChatHandler
         bool HandleDebugSetItemValueCommand(char* args);
         bool HandleDebugSetValueCommand(char* args);
         bool HandleDebugSpellCheckCommand(char* args);
+        bool HandleDebugSpellCoefsCommand(char* args);
         bool HandleDebugSpellModsCommand(char* args);
         bool HandleDebugUpdateWorldStateCommand(char* args);
         bool HandleDebugEnterVehicleCommand(char* args);
