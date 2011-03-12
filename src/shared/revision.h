b660d1094f1238ea61f845cdc5d0f1251f479ac4
Michael F <mefurth@miners.utep.edu>
2011-03-12 14:00:44 -0700

    Update

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "b796fb7500323a3c6140c22d783c9e9407a1f7cf"
+ #define REVISION_ID "012a3a5d3ebd781ce1c5bb62f58bf44245cc73fc"
  #define REVISION_DATE "*"
  #define REVISION_TIME "*"
 #endif // __REVISION_H__
